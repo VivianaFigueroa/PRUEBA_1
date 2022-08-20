20ecd5a894b91774622a5332a7416e9e3cb423ed
VivianaFigueroa <108564110+VivianaFigueroa@users.noreply.github.com>
2022-08-20 18:05:57 -0400

    Add files via upload

---
+++ Funciones_intermedias_I.py
@@ -0,0 +1,28 @@
+x = [ [5,2,3], [10,8,9] ] 
+
+estudiantes = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# 1.
+x[1][0] = 15
+print (x)
+
+# 2.
+estudiantes [0] ['last_name'] = 'Bryant'
+print (estudiantes)
+
+# 3.
+directorio_deportes['fútbol'] [0] = 'Andres'
+print (directorio_deportes)
+
+# 4.
+z[0] ['y'] = 30
+print (z)
