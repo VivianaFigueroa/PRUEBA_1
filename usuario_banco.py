bcb57f73162b000dd3f11cdd98baf31abc98ac91
VivianaFigueroa <108564110+VivianaFigueroa@users.noreply.github.com>
2022-11-11 23:46:59 -0300

    Add files via upload

--- usuario_banco.py
+++ usuario_banco.py
@@ -1,25 +1,64 @@
-from cuenta_bancaria import CuentaBancaria
+class CuentaBancaria:
 
-class usuario:
-    def __init__(self, balance = 200):
-        self.account = CuentaBancaria(balance=900)
+    def __init__(self, tasa_interes=0.15, balance =1000):
+        self.tasa_interes = tasa_interes
+        self.balance = balance
+
+    def deposito(self, amount):
+        print ('DEPOSITO')
+        self.balance = self.balance + amount
+
+    def retiro(self, amount):
+        print('RETIRO')
+        self.balance = self.balance - amount
+
+    def mostrar_info_cuenta(self):
+        print(f'BALANCE: {self.balance} \n Interes: {self.tasa_interes}')
+
+    def generar_interes(self):
+        print('GENERAR INTERES')
+        new_interes_amount = self.balance * self.tasa_interes
+        print (f' INTERES ADQUIRIDO: {new_interes_amount}')
+        self.balance = self.balance + new_interes_amount
+
+class Usuario:
+    def __init__(self, balance = 100):
+        self.account = CuentaBancaria(balance=1200)
 
     def deposito(self, amount):
         self.account.deposito(amount)
+        return self
 
     def retiro(self, amount):
         self.account.retiro(amount)
+        return self
     
     def mostrar_info_cuenta(self):
         self.account.mostrar_info_cuenta()
+        return self
 
 
-usuario1 = usuario()
-usuario2 = usuario()
+usuario1 = Usuario()
+usuario2 = Usuario()
+usuario3 = Usuario()
 
+print("Detalles movimientos usuario1")
 usuario1.deposito(300)
-usuario2.deposito(500)
-
-
+usuario1.deposito(200)
+usuario1.deposito(450)
+usuario1.retiro(320)
 usuario1.mostrar_info_cuenta()
+
+print("Detalles movimientos usuario2")
+usuario2.deposito(100)
+usuario2.deposito(250)
+usuario2.retiro(160)
+usuario2.retiro(220)
 usuario2.mostrar_info_cuenta()
+
+print("Detalles movimientos usuario3")
+usuario3.deposito(250)
+usuario3.retiro(160)
+usuario3.retiro(220)
+usuario3.retiro(70)
+usuario3.mostrar_info_cuenta()
